4e1d08710154006cad022291fa13dc17d118d56c
Anastasia <19748371+cptntotoro@users.noreply.github.com>
2025-07-17 22:40:05 +0300

    Верстка согласно ТЗ

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,120 @@
+import { useEffect, useState } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	ArticleStateType,
+	OptionType,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	currentState: ArticleStateType;
+	onApply: (newState: ArticleStateType) => void;
+	onReset: () => void;
+};
+
+export const ArticleParamsForm = ({
+	currentState,
+	onApply,
+	onReset,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] = useState<ArticleStateType>(currentState);
+
+	useEffect(() => {
+		setFormState(currentState);
+	}, [currentState]);
+
+	const handleChange =
+		(field: keyof typeof formState) => (option: OptionType) => {
+			setFormState((prev) => ({
+				...prev,
+				[field]: option,
+			}));
+		};
+
+	const handleApply = () => {
+		onApply(formState);
+		setIsOpen(false);
+	};
+
+	const handleReset = () => {
+		setFormState(currentState);
+		onReset();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+			<aside
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={handleChange('fontFamilyOption')}
+					/>
+
+					<RadioGroup
+						title='Размер шрифта'
+						name='font'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={handleChange('fontSizeOption')}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={handleChange('fontColor')}
+					/>
+
+					<Separator />
+
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={handleChange('backgroundColor')}
+					/>
+
+					<Select
+						title='Ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={handleChange('contentWidth')}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='button'
+							type='apply'
+							onClick={handleApply}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +16,34 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [stylesState, setStylesState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const handleApply = (newStyles: ArticleStateType) => {
+		setStylesState(newStyles);
+	};
+
+	const handleReset = () => {
+		setStylesState(defaultArticleState);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': stylesState.fontFamilyOption.value,
+					'--font-size': stylesState.fontSizeOption.value,
+					'--font-color': stylesState.fontColor.value,
+					'--container-width': stylesState.contentWidth.value,
+					'--bg-color': stylesState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				currentState={stylesState}
+				onApply={handleApply}
+				onReset={handleReset}
+			/>
 			<Article />
 		</main>
 	);
